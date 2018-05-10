03bce8aa68afa1b1e44fc227bc35f1899ef3ffca
Thomas Kacymarek <mulenatic@gmail.com>
2018-05-10 16:58:46 +0000

    A complete example for OTA and WiFiManager.

---
+++ BasicOTA/src/BasicOTA.ino
@@ -0,0 +1,79 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <ArduinoOTA.h>
+#include <WiFiManager.h>
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting");
+
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("esp8266-setup");
+  
+  
+  // WiFi.mode(WIFI_STA);
+  // WiFi.begin(ssid, password);
+  // while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+  //   Serial.println("Connection Failed! Rebooting...");
+  //   delay(5000);
+  //   ESP.restart();
+  // }
+
+  // Port defaults to 8266
+  ArduinoOTA.setPort(8266);
+
+  // Hostname default to esp8266-[ChipId]
+  ArduinoOTA.setHostname("NodeMCU1");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // ArduinoOTA.setPasswordHash(xxx);
+
+  ArduinoOTA.onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH) {
+	type == "sketch";
+      } else {
+	type = "filesystem";
+      }
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating" + type);
+    });
+
+  ArduinoOTA.onEnd([]() {
+      Serial.println("\nEND");
+    });
+
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / ( total / 100 )));
+    });
+
+  ArduinoOTA.onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) {
+	Serial.println("Auth failed");
+      } else if (error == OTA_BEGIN_ERROR) {
+	Serial.println("Begin failed");
+      } else if (error == OTA_CONNECT_ERROR) {
+	Serial.println("Connect failed");
+      } else if (error == OTA_RECEIVE_ERROR) {
+	Serial.println("Receive failed");
+      } else if (error == OTA_END_ERROR) {
+	Serial.println("End failed");
+      }
+    });
+
+  ArduinoOTA.begin();
+  Serial.println("Ready for business OTA");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());		   
+}
+
+void loop() {
+  ArduinoOTA.handle();
+}
+    
