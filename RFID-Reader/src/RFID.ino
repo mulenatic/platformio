c699852d0eaffd22ad7ffbb771f0835e659ef3b4
mulenatic <mulenatic@gmail.com>
2018-05-14 14:36:51 +0200

    NodeMCU can now speak to ServiceNow.

--- RFID-Reader/src/RFID.ino
+++ RFID-Reader/src/RFID.ino
@@ -1,3 +1,4 @@
+
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <ArduinoOTA.h>
@@ -10,9 +11,25 @@
 #define SS_PIN 2
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
+// Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory;
+MFRC522::MIFARE_Key key;
+
+WiFiClientSecure client;
+const char* host = "poc9.osnow.de";
+const int httpsPort = 443;
+
+const char* fingerprint = "dc a6 d8 b8 0a ea 7e 0b 32 77 14 05 2f 27 b2 90 32 b7 7e b2";
+
+//ESP8266WebServer server(80);
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Booting");
+
+  for(byte i=0; i<6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+
 
   SPI.begin();
   mfrc522.PCD_Init();
@@ -30,7 +47,13 @@ void setup() {
   // ArduinoOTA.setPassword("admin");
 
   // Password can be set with it's md5 value as well
+
   // ArduinoOTA.setPasswordHash(xxx);
+
+  //server.on("/cardId", HTTP_GET, handleGetCardId);
+  //server.begin();
+  //Serial.println("ESP8266WebServer started");
+
 
   ArduinoOTA.onStart([]() {
       String type;
@@ -74,28 +97,41 @@ void setup() {
 }
 
 void loop() {
+
   ArduinoOTA.handle();
+  readCardID();
+  
+  //server.handleClient();
+}
 
-  // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory;
-  MFRC522::MIFARE_Key key;
-  for(byte i=0; i<6; i++) {
-    key.keyByte[i] = 0xFF;
-  }
+void handleGetCardId() {
 
-  //some variable we need
-  byte block;
-  byte len;
-  MFRC522::StatusCode status;
+  //String cardId = readCardID();
+  //Serial.print("ID: ");
+  //Serial.println(cardId);
+  //server.send(200, "text/json", "{ \"success\": \"true\", \"cardId\": \"" + cardId + "\" }");
+
+}
+
+void readCardID() {
+
+  String cardId = "";
+  boolean isFinished = false;
+  //  while(!isFinished) {
+
+  yield();
 
   //--------------------
 
   // Look for new cards
   if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    //continue;
     return;
   }
 
   // Select one of the cards
   if ( ! mfrc522.PICC_ReadCardSerial()) {
+    //      continue;
     return;
   }
 
@@ -105,51 +141,75 @@ void loop() {
 
   mfrc522.PICC_DumpDetailsToSerial(&(mfrc522.uid)); // dump some details about the card
 
+  long code = 0;
+  for (byte i = 0; i < mfrc522.uid.size; i++ ) {
+    code = ((code + mfrc522.uid.uidByte[i]) * 10 );
+  }
+
+  cardId = String(code);
+
   //====================
 
-  Serial.print(F("Name: "));
 
-  byte buffer1[18];
-
-  block = 4;
-  len = 18;
-
-  //==================== Get first name
-  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 4, &key, &(mfrc522.uid));
-  if (status != MFRC522::STATUS_OK) {
-    Serial.print(F("Authentication failed: "));
-    Serial.println(mfrc522.GetStatusCodeName(status));
-    return;
-  }
-
-  status = mfrc522.MIFARE_Read(block, buffer1, &len);
-  if (status != MFRC522::STATUS_OK) {
-    Serial.print(F("Reading failed: "));
-    Serial.println(mfrc522.GetStatusCodeName(status));
-    return;
-  }
-
-  // Print first name
-  for (byte i = 0; i < 16; i++ ) {
-    if (buffer1[i] != 32) {
-      Serial.write(buffer1[i]);
-    }
-  }
-  Serial.println("");
-  Serial.println("Finished reading");
-  Serial.println("");
-
-  //--------------------
 
   Serial.println(F("\n**End Reading**\n"));
 
-  delay(1000); // change value if you want to read cards faster
+  isFinished = true;
+  yield();
 
-  mfrc522.PICC_HaltA();
-  mfrc522.PCD_StopCrypto1();
+  Serial.print("ID: ");
+  Serial.println(cardId);
+
+  if (!client.connect(host, httpsPort)) {
+    Serial.println("Connection to ServiceNow failed.");
+    return;
+  } else {
+    Serial.println("Connection to ServiceNow succeded");
+  }
+
+  if ( client.verify(fingerprint, host)) {
+    Serial.println("Certificates matches");
+  } else {
+    Serial.println("Certifiactes don't match");
+  }
+
+
+  String url = "/api/x_buergerbox/buergerboxrestapi/buergerbox/kartenid/" + cardId;
+
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+	       "Host: " + host + "\r\n" +
+	       "User-Agent: BuildFailureDetectorESP8266\r\n" + 
+	       "Authorization: Basic bm9kZU1DVToxMTEx\r\n" +
+	       "Connection: close\r\n\r\n");
+  unsigned long timeout = millis();
+  while(client.available() == 0) {
+    if(millis() - timeout > 5000) {
+      Serial.println(">>> Client timeout!");
+      client.stop();
+      return;
+    }
+  }
+
+  Serial.println("Send request, next wait for answer");
+
+  while(client.available()) {
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+
+  Serial.println();
+  Serial.println("Closing connection");
+  
+
+  delay(1000);
+
+    
+    
+  //  }
+  //  return cardId;
   
 }
-    
-
-
       
